8eb204a320fb742d1839ac17db0d8d50ced16799
stvnchng <sc111@rice.edu>
2021-10-06 14:34:49 -0500

    working checkpoint for -x flag

--- src/frontend/Parser.java
+++ src/frontend/Parser.java
@@ -1,5 +1,6 @@
 package frontend;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -8,7 +9,10 @@ import frontend.Token.Lexeme;
 
 public class Parser {
 
+    // IR for -r flag
     private final IR irList = new IR();
+    // Ops for -x flag
+    private final IR ops = new IR();
     private int errors = 0;
     private Iterator<Token> tokens;
     private Token current;
@@ -65,6 +69,9 @@ public class Parser {
                         int[] op1 = new int[]{ sr1 }, op2 = new int[]{     }, op3 = new int[]{ sr3 };
                         IR.Node operation = new IR.Node(opcode, op1, op2, op3);
                         irList.add(operation);
+                        int[] ops1 = new int[]{ sr1, -1, -1, Integer.MAX_VALUE }, ops2 = new int[]{     }, ops3 = new int[]{ sr3, -1, -1, Integer.MAX_VALUE };
+                        operation = new IR.Node(opcode, ops1, ops2, ops3);
+                        ops.add(operation);
                     }
                 }
             }
@@ -96,6 +103,9 @@ public class Parser {
                         int[] op1 = new int[]{ sr1 }, op2 = new int[]{     }, op3 = new int[]{ sr3 };
                         IR.Node operation = new IR.Node(Lexeme.loadI.getInt(), op1, op2, op3, true);
                         irList.add(operation);
+                        int[] ops1 = new int[]{ sr1, -1, -1, Integer.MAX_VALUE }, ops2 = new int[]{     }, ops3 = new int[]{ sr3, -1, -1, Integer.MAX_VALUE };
+                        operation = new IR.Node(Lexeme.loadI.getInt(), ops1, ops2, ops3, true);
+                        ops.add(operation);
                     }
                 }
             }
@@ -138,6 +148,9 @@ public class Parser {
                                 int[] op1 = new int[]{ sr1 }, op2 = new int[]{ sr2 }, op3 = new int[]{ sr3 };
                                 IR.Node operation = new IR.Node(opcode, op1, op2, op3);
                                 irList.add(operation);
+                                int[] ops1 = new int[]{ sr1, -1, -1, Integer.MAX_VALUE }, ops2 = new int[]{ sr2, -1, -1, Integer.MAX_VALUE }, ops3 = new int[]{ sr3, -1, -1, Integer.MAX_VALUE };
+                                operation = new IR.Node(opcode, ops1, ops2, ops3);
+                                ops.add(operation);
                             }
                         }
                     }
@@ -158,6 +171,8 @@ public class Parser {
                 int sr1 = Integer.parseInt(current.getLexeme());
                 IR.Node operation = new IR.Node(Lexeme.output.getInt(), new int[]{ sr1 });
                 irList.add(operation);
+                operation = new IR.Node(Lexeme.output.getInt(), new int[]{ sr1,  -1, -1, Integer.MAX_VALUE  });
+                ops.add(operation);
             }
         }
     }
@@ -179,49 +194,91 @@ public class Parser {
     int MAXLIVE = 0;
     int VRName = 0;
 
-    public LinkedList<IR.Node> getIR() {
-        return this.irList.getOps();
-    }
+    static final int SR = 0;
+    static final int VR = 1;
+    static final int NU = 3;
+
+    public String[] renamedOps;
 
     public void renameRegisters(LinkedList<IR.Node> ops) {
-        // First: Find distinct live ranges in code
-        // value is "live" from definition up to last use
         for (IR.Node op : ops) {
             if (op.getMaxSR() > maxSR) maxSR = op.getMaxSR();
         }
+        maxSR++;
         SRToVR = new int[maxSR];
         LU = new int[maxSR];
+        renamedOps = new String[ops.size()];
+        // initialize
         for (int idx = 0; idx < maxSR; idx++) {
             SRToVR[idx] = -1;
             LU[idx] = Integer.MAX_VALUE;
         }
         for (int i = ops.size() - 1; i > -1; i--) {
-            // load
-            if (ops.get(i).opcode == 0) {
-                // update and kill
-                if (SRToVR[ops.get(i).op3[0]] == -1) {
-                    SRToVR[ops.get(i).op3[0]] = VRName;
-                    VRName++;
+            IR.Node currOp = ops.get(i);
+            // LOAD, kill op3 then update op1, if ARITHOP also update op2
+            if (currOp.opcode == 0 || (currOp.opcode >= 3 && currOp.opcode <= 7)) {
+                updateTable(currOp.op3, i);
+                SRToVR[currOp.op3[SR]] = -1;
+                LU[currOp.op3[SR]] = Integer.MAX_VALUE;
+                updateTable(currOp.op1, i);
+                // ARITHOP, update op2
+                if (currOp.opcode >= 3) {
+                    updateTable(currOp.op2, i);
                 }
-                ops.get(i).op3[1] = SRToVR[ops.get(i).op3[0]];
-                ops.get(i).op3[3] = SRToVR[ops.get(i).op3[0]];
-
+                updateMAXLIVE();
             }
-            // store
-            else if (ops.get(i).opcode == 1) {
-                // don't need to initialize for store
-
+            // STORE, exception - don't need to kill op3, just update op1 and op2
+            else if (currOp.opcode == 1) {
+                updateTable(currOp.op1, i);
+                updateTable(currOp.op3, i);
+                updateMAXLIVE();
             }
-            // loadI
-            else if (ops.get(i).opcode == 2) {
-
+            // loadI, constant load into sr so just kill op3
+            else if (currOp.opcode == 2) {
+                updateTable(currOp.op3, i);
+                SRToVR[currOp.op3[SR]] = -1;
+                LU[currOp.op3[SR]] = Integer.MAX_VALUE;
+                updateMAXLIVE();
             }
-            // arithop
-            else if (ops.get(i).op1.length > 0 && ops.get(i).op2.length > 0 && ops.get(i).op3.length > 0) {
-
-            }
+//            System.out.println(i + ": " + Arrays.toString(SRToVR));
+//            System.out.println(i + ": " + Arrays.toString(LU));
+//            System.out.println("VRName: " + VRName);
+            updateRenamedOps(currOp, i);
         }
+        Arrays.stream(renamedOps).forEach(System.out::println);
+//        System.out.println("MAXLIVE: " + MAXLIVE);
     }
+
+    public void updateTable(int[] operand, int instructionNum) {
+        if (operand.length > 0) {
+            if (SRToVR[operand[SR]] == -1) {
+                SRToVR[operand[SR]] = VRName++;
+            }
+            operand[VR] = SRToVR[operand[SR]];
+            operand[NU] = SRToVR[operand[SR]];
+            LU[operand[SR]] = instructionNum;
+        } else System.out.println("Empty operand!");
+    }
+
+    public void updateMAXLIVE() {
+        long SRToVRCount = Arrays.stream(SRToVR).filter(reg -> reg != -1).count();
+        if (SRToVRCount > MAXLIVE) MAXLIVE = (int) SRToVRCount;
+    }
+
+    public void updateRenamedOps(IR.Node op, int i) {
+        String reg1 = "", reg2 = "", reg3 = "";
+        if (op.op1.length > 0 && op.op1[VR] != -1) reg1 = "r" + op.op1[VR];
+        else if (op.op1[SR] != -1) reg1 = String.valueOf(op.op1[SR]);
+        if (op.op2.length > 0 && op.op2[VR] != -1) reg2 = ", \tr" + op.op2[VR];
+        if (op.op3.length > 0 && op.op3[VR] != -1) reg3 = "r" + op.op3[VR];
+        String renamedOp = Token.Lexeme.valueOf(op.opcode) + "\t\t\t" + reg1 + reg2 + "\t=>\t" + reg3;
+        renamedOps[i]  = renamedOp;
+    }
+
+    public LinkedList<IR.Node> getIR() {
+        return this.ops.getOps();
+    }
+
     /**
      * -r flag
      */
